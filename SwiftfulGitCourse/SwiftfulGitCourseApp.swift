eca86ad7a621d6eb86ee0b6109364468173deacd
Rob K <info@aphotographycompany.com>
2024-11-11 12:24:57 -0800

    Initial Commit

---
+++ SwiftfulGitCourse/SwiftfulGitCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulGitCourseApp.swift
+//  SwiftfulGitCourse
+//
+//  Created by Robert Kjendlie on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulGitCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftfulGitCourseTests/SwiftfulGitCourseTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulGitCourseTests.swift
+//  SwiftfulGitCourseTests
+//
+//  Created by Robert Kjendlie on 11/10/24.
+//
+
+import Testing
+@testable import SwiftfulGitCourse
+
+struct SwiftfulGitCourseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
